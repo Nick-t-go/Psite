6dd24a322288ad55b5d517abeda73ef41d0bedb4
NT <nickt444@gmail.com>
2016-01-29 12:02:39 -0500

    fist commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    pageScroll();
+    $( window ).resize(function() {
+        $(document).unbind('scroll');
+        pageScroll();
+
+    });
+
+
+});
+
+
+var pageScroll = function() {
+    if ($(window).width() > 960) {
+        $(document).scroll(function () {
+            console.log($(window).width());
+            var $target = $('specialcontainer');
+            var winHeight = $(window).scrollTop();
+            var offsetArr = $('.specialcontainer').each(function () {
+                var top = $(this).offset().top;
+                var difference = winHeight - top;
+                $(this).find('.fixedContainer').css('top', difference);
+            })
+
+        });
+    } else{
+        var offsetArr2 = $('.specialcontainer').each(function () {
+            $(document).find('.fixedContainer').css('top', 'inherit');
+            $(document).find('.fixedContainer').addClass('row')
+        })
+    }
+};
+
+
+
+
+
